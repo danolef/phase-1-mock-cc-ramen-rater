0bf078d0a6173982aa1f1c4ea716d8ba9011e2e1
Dan <dt.lefever@gmail.com>
2021-10-21 12:26:18 -0600

    reterived data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // write your code here
+
+// Deliverables
+// [] see all ramen img in the div- render by 
+// [] click on an image and see info
+// [] create new ramen from submitting a form
+
+// Declear variables
+const url= " http://localhost:3000/ramens"
+
+// retreive data
+
+function reteriveRamen(){
+    fetch(url)
+    .then((res)=> res.json())
+    .then((ramenObjs) => createramen(ramenObjs)) 
+}
+
+reteriveRamen()
+
+// create data content
+
+function createramen(ramenObjs){
+    console.log(ramenObjs)
+}
+
+// rendurn the content
